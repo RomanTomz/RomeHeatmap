7cdbb3d3d8eb87f4d7c31570df43e4d41622276b
Tommaso Di Marcello <t.dimarcello@gmail.com>
2020-08-23 19:17:33 +0100

    Update reading.py

--- data/reading.py
+++ data/reading.py
@@ -1,3 +1,4 @@
-
+import pandas as pd
+import glob
 
 df = pd.concat([pd.read_csv(f,  sep=';', encoding='latin1') for f in glob.glob('/content/drive/My Drive/Colab Notebooks/Roma/Inc_Roma_2019/csv_incidenti_*.csv')])
